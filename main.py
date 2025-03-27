670e98a7ae42376203aca1d83fbd70ccfd982e54
Your Name <you@example.com>
2025-03-27 15:38:02 +0000

    Framing around the runner script

---
+++ main.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+if __name__ == "__main__":
+    pass
+else:
+    print("It looks like you've imported the executable script from elsewhere in the codebase!  Please ensure you only call this script from the command line.")
