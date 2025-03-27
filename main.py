5c9df19306eeed19ba15a25587e96b430c4a3ee2
Your Name <you@example.com>
2025-03-27 22:56:53 +0000

    Write help strings for three subcommands and their parameters

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 #! /usr/bin/env python3
+from argparse import ArgumentParser
+
 if __name__ == "__main__":
-    pass
+    parser = ArgumentParser(
+        prog='Zoo Manager Command-Line Utility',
+        description='Manages zoo files containing animal data'
+    )
+    parser.add_argument('-f', '--file', help='Name of the zoo file to modify.', default='zoo_data.txt')
+
+    subparsers = parser.add_subparsers(help='SUBCOMMANDS')
+
+    list_parser = subparsers.add_parser('list', help='Lists all animals in the zoo file.')
+
+    add_parser = subparsers.add_parser('add', help='Adds a new animal to the zoo file.')
+    add_parser.add_argument('name', help='Name of the individual animal.')
+    add_parser.add_argument('type', help='Type of animal to add.', choices=['cat','dog','bird'])
+
+    remove_parser = subparsers.add_parser('remove', help='Remove an animal by name OR remove animals by type.')
+    add_parser.add_argument('-n', '--name', help='Name of the individual animal.')
+    add_parser.add_argument('-t', '--type', help='Type of animal to add.', choices=['cat','dog','bird'])
+
+    arguments = parser.parse_args()
+    print(arguments)
 else:
     print("It looks like you've imported the executable script from elsewhere in the codebase!  Please ensure you only call this script from the command line.")
